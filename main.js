39a07daf7dbfbead99ebc33be610258729d50ca9
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-02 13:40:17 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,67 +1,44 @@
 // DRUM KIT (Modified Wes Bos JS30 Challenge)
 
 // Listen for key events on the page
-document.addEventListener("keydown", playSound);
+document.addEventListener("keydown", clickHandler);
 
 // Event Function
-function playSound(event) {
-    console.log(event.keyCode);
-
+function clickHandler(event) {
     if (event.keyCode == 65) { // "a"
-        // Play A-Clap
-        let audio = document.getElementById("clap");
-        audio.currentTime = 0;
-        audio.play();
-        document.getElementById("A").classList.add("playing");
+        playSound('clap','A');
     } else if (event.keyCode == 83) { // "s"
         // Play S-hihat
-        let audio = document.getElementById("hihat");
-        audio.currentTime = 0;
-        audio.play();
-        document.getElementById("S").classList.add("playing");
+       playSound('hihat','S');
     } else if (event.keyCode == 68) { // "d"
         // Play D-kick
-        let audio = document.getElementById("kick");
-        audio.currentTime = 0;
-        audio.play();
-        document.getElementById("D").classList.add("playing");
+        playSound('kick','D');
     } else if (event.keyCode == 70) { // "f"
         // Play F-Openhat
-        let audio = document.getElementById("openhat");
-        audio.currentTime = 0;
-        audio.play();
-        document.getElementById("F").classList.add("playing");
+        playSound('openhat','F');
     } else if (event.keyCode == 71) { // "g"
         // Play G-Boom
-        let audio = document.getElementById("boom");
-        audio.currentTime = 0;
-        audio.play();
-        document.getElementById("G").classList.add("playing");
+        playSound('boom','G');
     } else if (event.keyCode == 72) { // "h"
         // Play H-Ride
-        let audio = document.getElementById("ride");
-        audio.currentTime = 0;
-        audio.play();
-        document.getElementById("H").classList.add("playing");
+        playSound('ride','H');
     } else if (event.keyCode == 74) { // "j"
         // Play J-Snare
-        let audio = document.getElementById("snare");
-        audio.currentTime = 0;
-        audio.play();
-        document.getElementById("J").classList.add("playing");
+        playSound('snare','J');
     } else if (event.keyCode == 75) { // "k"
         // Play K-Tom
-        let audio = document.getElementById("tom");
-        audio.currentTime = 0;
-        audio.play();
-        document.getElementById("K").classList.add("playing");
+        playSound('tom','K');
     } else if (event.keyCode == 76) { // "l"
         // Play L-Tink
-        let audio = document.getElementById("tink");
+        playSound('tink','L');
+    }
+}
+
+function playSound(sound, key) {
+        let audio = document.getElementById(sound);
         audio.currentTime = 0;
         audio.play();
-        document.getElementById("L").classList.add("playing");
-    }
+        document.getElementById(key).classList.add("playing");
 }
 
 // Add transition end listeners
